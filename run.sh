b7bea7f905170c0e7d0492feeea5c96de5505fc5
David McCuskey <my-github@davidmccuskey.com>
2014-06-04 17:04:59 -0600

    update run.sh with comments for general use

--- run.sh
+++ run.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
+# this is just a simple shell script i used to run my project. it helps:
+# * patch the LUA_PATH for library lookup
+# * launch the project with my current command
+
+# Note: you'll probably want to change: project libs, lua version, or command
+
 # project specific dirs
-project_libs="./libs/?.lua;./libs/dmc_lua/?.lua;./data_services/?.lua;"
+project_libs="./libs/?.lua;./libs/dmc_corona/?.lua;./data_services/?.lua;"
 
 # get default LUA_PATH
 lua_path=$(lua5.1 -e "print(package.path)")
