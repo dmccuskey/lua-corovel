c1e7cba5765ec81b9a82a23a2142e0a6d79a6895
dmc <my-github@mccuskey.com>
2015-01-09 18:47:32 -0800

    first checkin for ssl shim

---
+++ corovel/plugin/openssl.lua
@@ -0,0 +1,67 @@
+--====================================================================--
+-- corovel/plugin/openssl.lua
+--
+-- Documentation: http://docs.davidmccuskey.com/display/docs/Lua+Corovel.lua
+--====================================================================--
+
+--[[
+
+The MIT License (MIT)
+
+Copyright (c) 2015 David McCuskey
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+--]]
+
+
+
+--====================================================================--
+--== Corovel : SSL Shim
+--====================================================================--
+
+-- Semantic Versioning Specification: http://semver.org/
+
+local VERSION = "0.1.0"
+
+
+
+--====================================================================--
+--== Imports
+
+local ssl = require 'ssl'
+
+
+
+--====================================================================--
+--== Setup, Constants
+
+
+assert( ssl, "ssl package not found" )
+
+-- fake loaded package
+package.loaded['plugin_luasec_ssl'] = ssl
+
+
+
+-- TODO: figure out what to return here
+-- maybe just true ?
+return {
+	ssl=ssl
+}
